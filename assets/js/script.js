3e1fad9b41ea3c159cbe540edd7522437c2f6816
Ahmed Negm <73791551+AhmdNgm@users.noreply.github.com>
2021-09-15 14:39:37 +0200

    EDIT

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+// const sections = document.querySelectorAll("section");
+// const navLi = document.querySelectorAll("nav .container ul li");
+// window.addEventListener("scroll", () => {
+//   let current = "";
+//   sections.forEach((section) => {
+//     const sectionTop = section.offsetTop;
+//     const sectionHeight = section.clientHeight;
+//     if (pageYOffset >= sectionTop - sectionHeight * 0.80) {
+//       current = section.getAttribute("id");
+//     }
+//   });
+
+//   navLi.forEach((li) => {
+//     li.classList.remove("active");
+//     if (li.classList.contains(current)) {
+//       li.classList.add("active");
+//     }
+//   });
+// });
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// learn
